73a703e7cda692d166788737267504127741b049
ngannguyen03 <baongan031205@gmail.com>
2026-05-22 13:41:40 +0700

    bo sung buoi 3

--- CMS.Backend/Controllers/HomeController.cs
+++ CMS.Backend/Controllers/HomeController.cs
@@ -1,32 +1,33 @@
-using System.Diagnostics;
-using CMS.Backend.Models;
+﻿using CMS.Data;
+using CMS.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CMS.Backend.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        // Inject DbContext vào Controller
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        // Hiển thị 3 bản tin mới nhất lên trang chủ
         public IActionResult Index()
         {
-            return View();
-        }
+            // LINQ: Kết hợp Include, OrderByDescending và Take để lấy đúng 3 bài mới nhất
+            var latestPosts = _context.Posts
+                                      .Include(p => p.Category) // Lấy kèm tên danh mục để hiển thị ở giao diện
+                                      .OrderByDescending(p => p.CreatedDate) // Sắp xếp ngày mới nhất lên đầu
+                                      .Take(3) // Chỉ lấy đúng 3 bản ghi đầu tiên
+                                      .ToList();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // Truyền danh sách 3 bài viết ra View trang chủ
+            return View(latestPosts);
         }
     }
 }
